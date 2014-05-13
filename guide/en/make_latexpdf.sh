505dd1e685853cfb4a0c9a9680acaf190a35267a
Ngoc Dao <ngocdaothanh@gmail.com>
2014-05-13 12:15:47 +0900

    latexpdf can't use .gif image

--- guide/en/make_latexpdf.sh
+++ guide/en/make_latexpdf.sh
@@ -2,4 +2,8 @@
 
 ln -s ../conf.py .
 ln -s ../Makefile .
+
+# latexpdf can't use .gif image
+sed -i.bak s/ajax_loading\.gif/ajax_loading\.png/g postback.rst
 make latexpdf
+mv postback.rst.bak postback.rst
