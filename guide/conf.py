2842f30845acfac205f0f75fcdbcc15b71819d3e
Ngoc Dao <ngocdaothanh@gmail.com>
2014-05-24 12:17:52 +0900

    Improve PDF version: Add logo, show pageref (for printing), set toc depth to 4

--- guide/conf.py
+++ guide/conf.py
@@ -184,20 +184,20 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = '../img/whale.png'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
 #latex_use_parts = False
 
 # If true, show page references after internal links.
-#latex_show_pagerefs = False
+latex_show_pagerefs = True
 
 # If true, show URL addresses after external links.
 #latex_show_urls = False
 
 # Additional stuff for the LaTeX preamble.
-#latex_preamble = ''
+latex_preamble = '\setcounter{tocdepth}{4}'
 
 # Documents to append as an appendix to all manuals.
 #latex_appendices = []
