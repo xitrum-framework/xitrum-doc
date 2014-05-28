8bdc2d028192df09fbbc69f3e395c4b31c562d80
Ngoc Dao <ngocdaothanh@gmail.com>
2014-05-28 19:14:11 +0900

    Fix #70 Simplify make_latexpdf.sh

--- guide/make_latexpdf.sh
+++ guide/make_latexpdf.sh
@@ -1,42 +1,28 @@
 #!/bin/sh
 
 make_latexpdf() {
-  cd en
+  cd $1
 
-  rm -f conf.py
-  cp ../conf.py .
+  rm -rf conf.py
+  rm -rf Makefile
+  ln -s ../conf.py .
   ln -s ../Makefile .
 
-  # latexpdf can't use .gif image
+  # PDF can't use .gif image
   sed -i.bak s/ajax_loading\.gif/ajax_loading\.png/g postback.rst
 
-  make latexpdf
+  if [ $1 = 'ja' ]
+  then
+    make latexpdfja
+  else
+    make latexpdf
+  fi
+
+  # Restore postback.rst
   mv postback.rst.bak postback.rst
 
   cd ..
 }
 
-make_latexpdfja() {
-  cd ja
-
-  rm -f conf.py
-  cp ../conf.py .
-  ln -s ../Makefile .
-
-  # Set language from None (English) to Japanese
-  sed -i.bak s/\#language=None/language=\'ja\'/g conf.py
-
-  # Oshida-san is the main author of the Japanese version
-  sed -i.bak s/Ngoc[[:space:]]Dao,[[:space:]]Takeharu[[:space:]]Oshida/押田　丈治、ダオ　ゴック/g conf.py
-
-  # latexpdf can't use .gif image
-  sed -i.bak s/ajax_loading\.gif/ajax_loading\.png/g postback.rst
-
-  make latexpdfja
-  mv postback.rst.bak postback.rst
-
-  cd ..
-}
-
-make_latexpdf
-make_latexpdfja
+make_latexpdf en
+make_latexpdf ja
